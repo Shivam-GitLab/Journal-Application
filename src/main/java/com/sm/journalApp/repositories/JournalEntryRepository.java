5f8d79e6a59246ba0cd1d1304de4ac6e9e11b45e
Shivam-GitLab <shivamkumarsaitm@gmail.com>
2025-07-14 23:17:52 +0530

    JournalEntryRepository Update

--- src/main/java/com/sm/journalApp/repositories/JournalEntryRepository.java
+++ src/main/java/com/sm/journalApp/repositories/JournalEntryRepository.java
@@ -5,6 +5,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository // Optional, but a good practice
 public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
 
