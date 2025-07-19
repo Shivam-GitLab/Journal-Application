a18a1ee5d7a61b1afe21cb9e6224baf55756aa92
Shivam-GitLab <shivamkumarsaitm@gmail.com>
2025-07-19 09:35:33 +0530

    Exception

--- src/main/java/com/sm/journalApp/controllers/JournalEntryController.java
+++ src/main/java/com/sm/journalApp/controllers/JournalEntryController.java
@@ -14,7 +14,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-
 /*
 ✅ @RestController
 
@@ -109,7 +108,6 @@ public class JournalEntryController {
     @PostMapping("/create-post/{userName}")
     public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry, @PathVariable String userName){
         try {
-
             myEntry.setDate(LocalDateTime.now());
             journalEntryService.saveEntry(myEntry,userName);
             return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
