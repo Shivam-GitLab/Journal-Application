0a3868363d9e6e312fdef9a51b92f3cbf9b6a648
Shivam-GitLab <shivamkumarsaitm@gmail.com>
2025-08-01 23:49:01 +0530

    Code

--- src/main/java/com/sm/journalApp/repositories/JournalEntryRepository.java
+++ src/main/java/com/sm/journalApp/repositories/JournalEntryRepository.java
@@ -8,5 +8,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository // Optional, but a good practice
 public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
-
 }
