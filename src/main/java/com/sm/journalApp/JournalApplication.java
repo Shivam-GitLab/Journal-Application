b30c3604ef920b483921461a92522d6415395618
Shivam-GitLab <shivam_223033@saitm.org>
2025-11-09 16:46:55 +0530

    pom update

--- src/main/java/com/sm/journalApp/JournalApplication.java
+++ src/main/java/com/sm/journalApp/JournalApplication.java
@@ -2,16 +2,14 @@ package com.sm.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 //@EnableTransactionManagement
-public class JournalApplication {	public static void main(String[] args) {
+public class JournalApplication {
+    public static void main(String[] args) {
         SpringApplication.run(JournalApplication.class, args);
 	}
-
 /*	@Bean
 	public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
 		return new MongoTransactionManager(dbFactory);
 	}*/
-
 }
